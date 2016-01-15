9fd32e974b0ba52d9eeebad143fc817150aaf321
Tomas Lang <tomas@loftdigital.com>
2016-01-15 20:12:51 +0100

    Move styles to separate file

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@ var gulp = require('gulp')
 var browserify = require('browserify');
 var babelify = require('babelify');
 
+var sass = require('gulp-sass');
+
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
-
 
 function bundleJs(bundler) {
 	return bundler.bundle()
@@ -20,14 +21,22 @@ function bundleJs(bundler) {
 		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(uglify())
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('dist'))
+		.pipe(gulp.dest('dist/js'))
 }
 
-gulp.task('default', function () {
-	var bundler = browserify('src/app.js', { debug: true })
+gulp.task('scripts', function () {
+	var bundler = browserify('src/js/app.js', { debug: true })
 		.transform(babelify, {
 			'presets': ['es2015']
 		});
 
 	return bundleJs(bundler);
 });
+
+gulp.task('styles', function() {
+	gulp.src('src/scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default', ['scripts', 'styles']);
