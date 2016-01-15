026ba43dbcbf9126893844f403821a5a9da05cbd
Tomas Lang <tomas@loftdigital.com>
2016-01-15 23:28:25 +0100

    ColorPallete1: add convergence condition

--- src/js/colorPallete/ColorPallete1.js
+++ src/js/colorPallete/ColorPallete1.js
@@ -2,11 +2,17 @@
 
 import Color from '../Color';
 
+const BLACK_COLOR = new Color(0, 0, 0, 255);
+
 export default class ColorPallete1
 {
 
 	computeColor(iter, maxIter, Tr, Ti)
 	{
+		if (iter == maxIter) { // converged
+			return BLACK_COLOR
+		}
+
 		return Color.fromHSL(0.10, 0.9, iter / maxIter);
 	}
 
