7e41f7f3a0950890e0d114b28b09cd0ff051e0d5
Tomas Lang <tomas@loftdigital.com>
2016-01-15 19:37:11 +0100

    Gulp: use browserify and babelify

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,33 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+
+var browserify = require('browserify');
+var babelify = require('babelify');
+
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
+
+
+function bundleJs(bundler) {
+	return bundler.bundle()
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('dist'))
+		.pipe(rename('app.min.js'))
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dist'))
+}
 
 gulp.task('default', function () {
-  return gulp.src('src/**/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(concat('app.js'))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'));
+	var bundler = browserify('src/app.js', { debug: true })
+		.transform(babelify, {
+			'presets': ['es2015']
+		});
+
+	return bundleJs(bundler);
 });
